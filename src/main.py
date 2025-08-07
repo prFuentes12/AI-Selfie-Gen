4bf3b1786b0723b2582b7653755643b0a775f5d8
Fernando <fdofuentescarrasco@gmail.com>
2025-08-07 10:26:03 +0200

    New image-generator endpoint

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import helpers
 
 app= FastAPI()
 
 
 @app.get("/")
 def read_index():
+    # helpers.generate_image()
     return {"hello" : "world"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str,None] = None):
-    return {"item_id" : item_id, "q" : q}
+
+class ImageGenerationRequest (BaseModel):
+    prompt : str
+
+
+@app.post("/generate")
+def generate_image(data: ImageGenerationRequest):
+    try:
+
+        pred_results= helpers.generate_image(data.prompt)
+        return pred_results
+    except Exception as e: 
+        raise HTTPException(status_code = 500, detail= str(e))
+    
